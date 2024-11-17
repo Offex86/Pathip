ac73cfef41bb31e92848e594280f31886ac828d0
Offex86 <factblink514@gmail.com>
2024-11-17 22:43:10 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,7 +15,7 @@ let subProtocol = 'https';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
 // Example:  user:pass@host:port  or  host:port
-let socks5Address = '115.240.101.49:8080';
+let socks5Address = 'ntop:vip@wifi.getnova.tech:80';
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
